3bae92bc6fdb6e06753a8b927316a4d77fb7a2cd
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-19 20:17:13 -0500

    added questions, tested in replit

---
+++ index.js
@@ -0,0 +1,76 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${fileName} generated successfully!`);
+    }
+  });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
+
+// Function call to initialize app
+init();
